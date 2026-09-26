f2498dc7961a80d0598093d6570fff1b28ad6681
Chiranjit Das <souravraj78i8@gmail.com>
2026-09-26 11:40:01 +0530

    add Next.js TypeScript environment declarations

---
+++ admin/next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited.
+// It is generated/managed by Next.js for TypeScript projects.
